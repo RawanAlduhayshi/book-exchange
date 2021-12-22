67dd492a8fbf4a5e440ac75106a6a3b76cb309be
Rawan <Rawan.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-12-22 14:22:50 +0300

    edit pre-built email\password authentication

--- app/src/main/java/com/rawanalduhyshi/bookexchange/MainActivity.kt
+++ app/src/main/java/com/rawanalduhyshi/bookexchange/MainActivity.kt
@@ -2,12 +2,15 @@ package com.rawanalduhyshi.bookexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,11 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
 
+//        var button2 = findViewById<Button>(R.id.save)
+//
+//        button2.setOnClickListener {
+//            save()
+//        }
         val signInLauncher = registerForActivityResult(FirebaseAuthUIActivityResultContract())
         { res ->
             this.onSignInResult(res)
@@ -50,5 +58,24 @@ class MainActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
         }
+        }
+
+
+        fun save(){
+            val db = Firebase.firestore
+            val user = hashMapOf(
+                "first" to "Email",
+                "last" to "Password"
+            )
+
+// Add a new document with a generated ID
+            db.collection("login")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("TAG", "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w("TAG", "Error adding document", e)
+                }
+        }
     }
-}
